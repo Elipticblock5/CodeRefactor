3c0a0fc191751ab5ee9113477f045e63714796d6
Nate Johnson <nbjohnson@gmail.com>
2021-01-31 15:28:36 -0700

    background-image

---
+++ watch-that-box/javascript.js
@@ -0,0 +1,18 @@
+document.getElementById("blueBtn").addEventListener("click", function(){
+
+    document.getElementById("box").style.color = blue;
+
+});
+
+document.getElementById("growBtn").addEventListener("click", function(){
+
+    document.getElementById("box").style.height = "250px";
+
+});
+
+document.getElementById("resetBtn").addEventListener("click", function(){
+
+    document.getElementById("box").style.height = "150px";
+
+});
+
